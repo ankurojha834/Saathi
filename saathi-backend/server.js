1624155d301d92780fc236dc4c5c3a230c188d25
Ankur Ojha <ankurojha175@gmail.com>
2025-09-20 23:13:14 +0530

    backend : allow all

--- saathi-backend/server.js
+++ saathi-backend/server.js
@@ -10,10 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({
-  origin: 'https://saathi-supporter.vercel.app/' || 'http://localhost:3000',
-  credentials: true
-}));
+
+app.use(cors());
+
 app.use(express.json({ limit: '10mb' }));
 
 // Rate limiting
