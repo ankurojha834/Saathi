13760723ec5e8d7e4b67f9dd1a336f055488bcd2
Ankur Ojha <ankurojha175@gmail.com>
2025-09-20 23:08:09 +0530

    backend : added the deployed frontend url

--- saathi-backend/server.js
+++ saathi-backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: 'https://saathi-supporter.vercel.app/' || 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
